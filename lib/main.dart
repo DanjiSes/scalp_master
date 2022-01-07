af2eeec61dc49e8910f6bb2713c2798116e017e4
Savchenko <danil.savchenko.dev@gmail.com>
2022-01-07 03:51:54 +0300

    Add base row widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,83 +10,99 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-        padding: const EdgeInsets.all(32),
-        child: Row(
-          children: [
-            Expanded(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: const Text(
-                      'Oeschinen Lake Campground!',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )),
-                Text('Kandersteg, Switzerland',
-                    style: TextStyle(color: Colors.grey[500]))
-              ],
-            )),
-            Icon(Icons.star, color: Colors.red[500]),
-            const Text('41')
-          ],
-        ));
-
-    Column _buildButtonColumn(Color color, IconData icon, String label) {
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(icon, color: color),
-          Container(
-            margin: const EdgeInsets.only(top: 8),
-            child: Text(
-              label,
-              style: TextStyle(
-                  fontSize: 12, fontWeight: FontWeight.w400, color: color),
-            ),
-          )
-        ],
-      );
-    }
-
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-        _buildButtonColumn(color, Icons.share, 'SHARE'),
-      ],
-    );
-
-    Widget textSection = const Padding(
-        padding: EdgeInsets.all(32),
-        child: Text(
-          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-          'Alps. Situated 1,578 meters above sea level, it is one of the '
-          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-          'half-hour walk through pastures and pine forest, leads you to the '
-          'lake, which warms to 20 degrees Celsius in the summer. Activities '
-          'enjoyed here include rowing, and riding the summer toboggan run.',
-          softWrap: true,
-        ));
-
     return MaterialApp(
       title: 'Scalping Helper by Savchenko.dev',
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
       home: Scaffold(
-          appBar: AppBar(title: const Text('Scalping on Binance')),
-          body: ListView(
-            children: [
-              Image.asset('images/lake.jpg',
-                  width: 600, height: 240, fit: BoxFit.cover),
-              titleSection,
-              buttonSection,
-              textSection
-            ],
-          )),
+          body: Container(
+        width: 250,
+        child: ListView(
+          children: const [
+            PriceItemWidget(
+              price: 45.432,
+              color: Colors.green,
+              volume: '4M',
+            ),
+            Divider(thickness: 1, height: 1),
+            PriceItemWidget(
+              price: 45.432,
+              color: Colors.red,
+              volume: '4M',
+            ),
+            Divider(thickness: 1, height: 1)
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+class PriceItemWidget extends StatefulWidget {
+  const PriceItemWidget(
+      {Key? key,
+      required this.volume,
+      required this.price,
+      required this.color})
+      : super(key: key);
+
+  final String volume;
+  final double price;
+  final Color color;
+
+  @override
+  _PriceItemWidgetState createState() => _PriceItemWidgetState();
+}
+
+class _PriceItemWidgetState extends State<PriceItemWidget> {
+  bool _highlighted = false;
+
+  void _highlightOn(PointerEvent details) {
+    setState(() {
+      _highlighted = true;
+    });
+  }
+
+  void _highlightOff(PointerEvent details) {
+    setState(() {
+      _highlighted = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(fontSize: 12);
+    var bgColor = widget.color.withOpacity(_highlighted ? 0.6 : 0.5);
+
+    return MouseRegion(
+      onEnter: _highlightOn,
+      onExit: _highlightOff,
+      child: GestureDetector(
+          child: IntrinsicHeight(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+                child: Container(
+              color: bgColor,
+              padding: const EdgeInsets.all(4),
+              child: Text(
+                widget.volume,
+                style: textStyle,
+              ),
+            )),
+            const VerticalDivider(width: 1, thickness: 1),
+            Expanded(
+                child: Container(
+              color: bgColor,
+              padding: const EdgeInsets.all(4),
+              child: Text(
+                widget.price.toString(),
+                style: textStyle,
+              ),
+            )),
+          ],
+        ),
+      )),
     );
   }
 }
